fabe492101133f09c2fc5f4581158c0ec35e08e2
jaymin-developer <kjmkjm822@naver.com>
2022-05-28 03:50:16 +0900

    fix/#14/transformTSX searchFirend finished

--- withchat-client/src/components/units/chatting/chattingRooms/chattingRooms.Container.tsx
+++ withchat-client/src/components/units/chatting/chattingRooms/chattingRooms.Container.tsx
@@ -1,25 +1,53 @@
+import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from "@mui/icons-material/Search";
 import * as S from "./ChattingRoom.Styles";
 
 export default function ChattingRoomList() {
+  const [currentTab, setCurrentTab] = useState(0);
   const chattingList = [
     { id: "1", name: "1번방" },
     { id: "2", name: "2번방" },
-    { id: "3", name: "3번방" },
+    { id: "3", name: "리코드레코드" },
   ];
+
+  const onClickSelectTab = (index: number) => {
+    setCurrentTab(index);
+  };
 
   return (
     <>
       <S.ChattingRoomWrapper>
         <S.ChattingRoomDmList src="/LOGO_WC.png" alt="DM 리스트" title="DM" />
         <S.ChattingRoomLine />
-        {chattingList.map((el) => (
-          <S.CurrentChattingRoom key={el.id} title={el.name}>
-            <div className="currentRoom" />
+        {chattingList.map((el, index) => (
+          <S.CurrentChattingRoom
+            className="transitionTap"
+            menuIndex={index}
+            key={el.id}
+            title={el.name}
+            currentTab={currentTab}
+            onClick={() => onClickSelectTab(index)}
+          >
             <div>{el.name}</div>
           </S.CurrentChattingRoom>
         ))}
-        <S.FindChattingRoom title="채팅방 추가">✚</S.FindChattingRoom>
-        <S.AddChattingRoom title="채팅방 찾기">◉</S.AddChattingRoom>
+        <S.FindChattingRoom
+          menuIndex={-1}
+          currentTab={chattingList.length}
+          title="채팅방 추가"
+          onClick={() => onClickSelectTab(chattingList.length)}
+        >
+          <AddIcon fontSize="large" />
+        </S.FindChattingRoom>
+        <S.AddChattingRoom
+          menuIndex={-1}
+          currentTab={chattingList.length + 1}
+          title="채팅방 찾기"
+          onClick={() => onClickSelectTab(chattingList.length + 1)}
+        >
+          <SearchIcon fontSize="large" />
+        </S.AddChattingRoom>
       </S.ChattingRoomWrapper>
     </>
   );
