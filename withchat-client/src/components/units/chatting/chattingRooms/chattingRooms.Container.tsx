e40128868755b5b639eb736fecdef60c13ee8fb6
jaymin-developer <kjmkjm822@naver.com>
2022-05-28 15:16:40 +0900

    middleCheck

--- withchat-client/src/components/units/chatting/chattingRooms/chattingRooms.Container.tsx
+++ withchat-client/src/components/units/chatting/chattingRooms/chattingRooms.Container.tsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import SearchIcon from "@mui/icons-material/Search";
 import * as S from "./chattingRoom.Styles";
+import axios from "axios";
 
 export default function ChattingRoomList() {
   const [currentTab, setCurrentTab] = useState(0);
-  const chattingList = [
-    { id: "1", name: "1번방" },
-    { id: "2", name: "2번방" },
-    { id: "3", name: "리코드레코드" },
-  ];
+  const [chattingList, setChattingList] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchMyChattingRooms = () => {
+      axios
+        .get("https://backend.withchat.site/chatting-room", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        })
+        .then((res) => {
+          setChattingList(res.data.result);
+          if (res.status === 201) console.log(res.data);
+        })
+        .catch((err) => console.log(err));
+    };
+    fetchMyChattingRooms();
+  }, []);
 
   const onClickSelectTab = (index: number) => {
     setCurrentTab(index);
@@ -20,18 +34,6 @@ export default function ChattingRoomList() {
       <S.ChattingRoomWrapper>
         <S.ChattingRoomDmList src="/LOGO_WC.png" alt="DM 리스트" title="DM" />
         <S.ChattingRoomLine />
-        {chattingList.map((el, index) => (
-          <S.CurrentChattingRoom
-            className="transitionTap"
-            menuIndex={index}
-            key={el.id}
-            title={el.name}
-            currentTab={currentTab}
-            onClick={() => onClickSelectTab(index)}
-          >
-            <div>{el.name}</div>
-          </S.CurrentChattingRoom>
-        ))}
         <S.FindChattingRoom
           menuIndex={-1}
           currentTab={chattingList.length}
@@ -40,6 +42,7 @@ export default function ChattingRoomList() {
         >
           <AddIcon fontSize="large" />
         </S.FindChattingRoom>
+
         <S.AddChattingRoom
           menuIndex={-1}
           currentTab={chattingList.length + 1}
@@ -48,6 +51,21 @@ export default function ChattingRoomList() {
         >
           <SearchIcon fontSize="large" />
         </S.AddChattingRoom>
+        <S.ChattingRoomLine />
+        <S.ChattingRoomBox>
+          {chattingList.map((el: any, index: number) => (
+            <S.CurrentChattingRoom
+              className="transitionTap"
+              menuIndex={index}
+              key={el.id}
+              title={el.name}
+              currentTab={currentTab}
+              onClick={() => onClickSelectTab(index)}
+            >
+              <div>{el.name}</div>
+            </S.CurrentChattingRoom>
+          ))}
+        </S.ChattingRoomBox>
       </S.ChattingRoomWrapper>
     </>
   );
