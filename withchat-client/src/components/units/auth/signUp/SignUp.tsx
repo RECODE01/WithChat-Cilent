bdcd4b21988f790a8e076af0afc488757cbba2f7
jaymin-developer <kjmkjm822@naver.com>
2022-06-01 14:58:48 +0900

    fix/#58/refactoring header

--- withchat-client/src/components/units/auth/signUp/SignUp.tsx
+++ withchat-client/src/components/units/auth/signUp/SignUp.tsx
@@ -1,92 +1,125 @@
-import * as S from '../Auth.Styles';
-import axios from 'axios';
-import { useForm } from 'react-hook-form';
-import { ISignUpFormData } from '../Auth.Types';
-import { useNavigate } from 'react-router-dom';
+import * as S from "../Auth.Styles";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { ISignUpFormData } from "../Auth.Types";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const navigate = useNavigate()
-    const onSubmitSignUp = (data : ISignUpFormData) => {
-        const variables = {
-                email: data.email,
-                name: data.name,
-                password: data.password,
-                nickName: data.nickName
-            } 
-        axios.post("https://backend.withchat.site/users", {}, {params: variables, headers: {
-            "Content-Type": "application/json",
-        } ,
-        }).then((res) => {
-            if(res.status === 201)
-            console.log(res)
-            navigate('/')
-            alert("메일이 발송되었습니다. 인증 후 로그인해주세요.")
-        }).catch((reason: any) => {
-            alert(reason.response.data.message)
-        });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+  const onSubmitSignUp = (data: ISignUpFormData) => {
+    const variables = {
+      email: data.email,
+      name: data.name,
+      password: data.password,
+      nickName: data.nickName,
     };
+    console.log(variables);
+    axios
+      .post(
+        "https://backend.withchat.site/users",
+        {},
+        {
+          params: variables,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        if (res.status === 201) console.log(res);
+        navigate("/");
+        alert("메일이 발송되었습니다. 인증 후 로그인해주세요.");
+      })
+      .catch((reason: any) => {
+        alert(reason.response.data.message);
+      });
+  };
 
-    return (
-        <S.AuthContentsWrapper>
-            <S.AuthContentsTitle>
-                <span>With Chat</span> SignUp
-             </S.AuthContentsTitle>
-             <S.AuthContentsSubTitle>
-                Please register as a member to log in <span>with chat.</span>
-             </S.AuthContentsSubTitle>
-            <form onSubmit={
-                handleSubmit && 
-                onSubmitSignUp && 
-                handleSubmit(onSubmitSignUp)
-            }>
-                <S.InputBox>
-                    <S.AuthInput 
-                        errorStatus={errors.email} 
-                        autoComplete='off' 
-                        type="text" 
-                        placeholder={errors.email ? "🚫  한 글자 이상 입력해주세요." : "사용할 이메일을 입력하세요."} 
-                        {...register("email",{ required: true })} 
-                    />
-                </S.InputBox>
-                <S.InputBox>
-                    <S.AuthInput 
-                        errorStatus={errors.password} 
-                        autoComplete='off' 
-                        type="password" 
-                        placeholder={errors.password ? "🚫  한 글자 이상 입력해주세요." : "사용할 비밀번호를 입력하세요."} 
-                        {...register("password",{ required: true })} 
-                    />
-                </S.InputBox>
-                <S.InputBox>
-                    <S.AuthInput 
-                        errorStatus={errors.passwordCheck} 
-                        autoComplete='off' 
-                        type="password" 
-                        placeholder={errors.passwordCheck ? "🚫  한 글자 이상 입력해주세요." : "비밀번호 확인을 입력하세요."} 
-                        {...register("passwordCheck",{ required: true })} 
-                    />
-                </S.InputBox>
-                <S.InputBox>
-                    <S.AuthInput 
-                        errorStatus={errors.name} 
-                        autoComplete='off' 
-                        type="text" 
-                        placeholder={errors.name ? "🚫  한 글자 이상 입력해주세요." : "본인의 이름을 입력하세요."} 
-                        {...register("name",{ required: true })} 
-                    />
-                </S.InputBox>
-                <S.InputBox>
-                    <S.AuthInput 
-                        errorStatus={errors.nickName} 
-                        autoComplete='off' 
-                        type="text" 
-                        placeholder={errors.nickName ? "🚫  한 글자 이상 입력해주세요." : "사용할 유저 명을 입력하세요."} 
-                        {...register("nickName",{ required: true })} 
-                    />
-                </S.InputBox>
-                <S.AuthButton type='submit'>회원 가입</S.AuthButton>
-            </form>
-        </S.AuthContentsWrapper>
-    );
+  return (
+    <S.AuthContentsWrapper>
+      <S.AuthContentsTitle>
+        <span>With Chat</span> SignUp
+      </S.AuthContentsTitle>
+      <S.AuthContentsSubTitle>
+        Please register as a member to log in <span>with chat.</span>
+      </S.AuthContentsSubTitle>
+      <form
+        onSubmit={
+          handleSubmit && onSubmitSignUp && handleSubmit(onSubmitSignUp)
+        }
+      >
+        <S.InputBox>
+          <S.AuthInput
+            errorStatus={errors.email}
+            autoComplete="off"
+            type="text"
+            placeholder={
+              errors.email
+                ? "🚫  한 글자 이상 입력해주세요."
+                : "사용할 이메일을 입력하세요."
+            }
+            {...register("email", { required: true })}
+          />
+        </S.InputBox>
+        <S.InputBox>
+          <S.AuthInput
+            errorStatus={errors.password}
+            autoComplete="off"
+            type="password"
+            placeholder={
+              errors.password
+                ? "🚫  한 글자 이상 입력해주세요."
+                : "사용할 비밀번호를 입력하세요."
+            }
+            {...register("password", { required: true })}
+          />
+        </S.InputBox>
+        <S.InputBox>
+          <S.AuthInput
+            errorStatus={errors.passwordCheck}
+            autoComplete="off"
+            type="password"
+            placeholder={
+              errors.passwordCheck
+                ? "🚫  한 글자 이상 입력해주세요."
+                : "비밀번호 확인을 입력하세요."
+            }
+            {...register("passwordCheck", { required: true })}
+          />
+        </S.InputBox>
+        <S.InputBox>
+          <S.AuthInput
+            errorStatus={errors.name}
+            autoComplete="off"
+            type="text"
+            placeholder={
+              errors.name
+                ? "🚫  한 글자 이상 입력해주세요."
+                : "본인의 이름을 입력하세요."
+            }
+            {...register("name", { required: true })}
+          />
+        </S.InputBox>
+        <S.InputBox>
+          <S.AuthInput
+            errorStatus={errors.nickName}
+            autoComplete="off"
+            type="text"
+            placeholder={
+              errors.nickName
+                ? "🚫  한 글자 이상 입력해주세요."
+                : "사용할 유저 명을 입력하세요."
+            }
+            {...register("nickName", { required: true })}
+          />
+        </S.InputBox>
+        <S.AuthButton type="submit">회원 가입</S.AuthButton>
+      </form>
+    </S.AuthContentsWrapper>
+  );
 }
--- withchat-client/src/components/units/createChattingRoom/createChattingRoom.presenter.tsx
+++ withchat-client/src/components/units/createChattingRoom/createChattingRoom.presenter.tsx
@@ -1,24 +1,35 @@
-import * as JH from './createChattingRoom.styles'
-export default function CreateChattingRoomUI(props: any){
-
-    return(
-        <JH.Background>
-
-        <JH.Wrapper>
-            <div className='Title'>채팅방 개설</div>
-            <div className='InputWrapper'>
-                <p>채팅방 이름</p>
-                <input className='InputTag' placeholder="채팅방 이름을 입력해보세요!" onChange={props.onChangeRoomName}/>
-            </div>
-            <form className='InputWrapper'>
-                <p>채팅방 대표 사진</p>
-                <input type='file' className='InputTag' placeholder="채팅방을 대표할 사진을 등록해보세요!" onChange={props.onClickFileUpload}/>
-            </form>
-            <div className='ButtonWrapper'>
-                <button className='Button' onClick={props.onClickCreateChattingRoom}>개설하기</button>
-                <button className='Button' onClick={props.onClickOpenCreateModal}>취소하기</button>
-            </div>
-        </JH.Wrapper>
-        </JH.Background>
-    )
+import * as JH from "./createChattingRoom.styles";
+export default function CreateChattingRoomUI(props: any) {
+  return (
+    <JH.Background>
+      <JH.Wrapper>
+        <div className="Title">서버 개설</div>
+        <div className="InputWrapper">
+          <p>서버 이름</p>
+          <input
+            className="InputTag"
+            placeholder="서버 이름을 입력해보세요!"
+            onChange={props.onChangeRoomName}
+          />
+        </div>
+        <form className="InputWrapper">
+          <p>서버 대표 사진</p>
+          <input
+            type="file"
+            className="InputTag"
+            placeholder="서버를 대표할 사진을 등록해보세요!"
+            onChange={props.onClickFileUpload}
+          />
+        </form>
+        <div className="ButtonWrapper">
+          <button className="Button" onClick={props.onClickCreateChattingRoom}>
+            개설하기
+          </button>
+          <button className="Button" onClick={props.onClickOpenCreateModal}>
+            취소하기
+          </button>
+        </div>
+      </JH.Wrapper>
+    </JH.Background>
+  );
 }
